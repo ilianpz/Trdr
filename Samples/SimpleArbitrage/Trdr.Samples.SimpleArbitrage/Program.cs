ebe5d978849935c8e21eb63721be3809718ca799
ilian <ilianpinzon@gmail.com>
2023-12-06 22:27:16 +1100

    Add sample strategy

---
+++ Samples/SimpleArbitrage/Trdr.Samples.SimpleArbitrage/Program.cs
@@ -0,0 +1,21 @@
+﻿using Trdr.Samples.SimpleArbitrage;
+using Binance = Trdr.Connectivity.Binance.Connection;
+using CoinJar = Trdr.Connectivity.CoinJar.Connection;
+
+var binance = Binance.Create();
+await binance.Connect();
+
+var coinJar = CoinJar.Create();
+await coinJar.Connect();
+
+const string symbol = "XRPUSDT";
+
+var coinJarTicker = coinJar.SubscribeTicker(symbol);
+var binanceTicker = binance.SubscribeTicker(symbol);
+
+var strategy = new SimpleArbitrageStrategy(
+    coinJarTicker,
+    binanceTicker,
+    buy => Console.WriteLine($"Buy at Binance @ {buy}"),
+    sell => Console.WriteLine($"Sell at CoinJar @ {sell}"));
+await strategy.Run();
---
+++ Samples/SimpleArbitrage/Trdr.Samples.SimpleArbitrage/SimpleArbitrageStrategy.cs
@@ -0,0 +1,59 @@
+﻿using Trdr.Connectivity.Binance;
+using Trdr.Connectivity.CoinJar.Phoenix;
+
+namespace Trdr.Samples.SimpleArbitrage;
+
+public sealed class SimpleArbitrageStrategy
+{
+    private readonly IAsyncEnumerable<TickerPayload> _coinJarTicker;
+    private readonly IAsyncEnumerable<Ticker> _binanceTicker;
+    private readonly Action<decimal> _buyAtBinance;
+    private readonly Action<decimal> _sellAtCoinJar;
+
+    public SimpleArbitrageStrategy(
+        IAsyncEnumerable<TickerPayload> coinJarTicker,
+        IAsyncEnumerable<Ticker> binanceTicker,
+        Action<decimal> buyAtBinance,
+        Action<decimal> sellAtCoinJar)
+    {
+        _coinJarTicker = coinJarTicker ?? throw new ArgumentNullException(nameof(coinJarTicker));
+        _binanceTicker = binanceTicker ?? throw new ArgumentNullException(nameof(binanceTicker));
+        _buyAtBinance = buyAtBinance ?? throw new ArgumentNullException(nameof(buyAtBinance));
+        _sellAtCoinJar = sellAtCoinJar ?? throw new ArgumentNullException(nameof(sellAtCoinJar));
+    }
+
+    public async Task Run(CancellationToken cancellationToken = default)
+    {
+        var sentinel = new Sentinel();
+
+        // Subscribe to Binance's ticker and store its ask everytime it's updated.
+        decimal buy = 0;
+        sentinel.Subscribe(_binanceTicker, ticker => buy = ticker.Ask);
+
+        // Subscribe to CoinJar's ticker and store its bid everytime it's updated.
+        decimal sell = 0;
+        sentinel.Subscribe(_coinJarTicker, ticker => sell = ticker.Bid);
+
+        sentinel.Start(); // Start the subscriptions
+
+        // This simple strategy waits for an arbitrage opportunity by buying low at Binance
+        // and selling high at CoinJar.
+        //
+        // Note: this is a toy trading strategy that will not work in the real world.
+        // It also ignores transaction fees and bid/ask quantities.
+        // This only serves to illustrate how the framework is meant to be used.
+
+        while (true)
+        {
+            await sentinel.Watch(
+                () =>
+                    sell > 0 && buy > 0 && // <-- Wait for both events
+                    sell - buy > 0.002m,
+                cancellationToken);
+
+            _buyAtBinance(buy);
+            _sellAtCoinJar(sell);
+        }
+        // ReSharper disable once FunctionNeverReturns
+    }
+}
